8acb12d159ab696870e6297ee609028baa6a1185
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-12-04 21:46:50 +0530

    use-effect-todo

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -26,7 +26,7 @@ const createTodo = (title) => {
 function Todos() {
   const [isLoading, setIsLoading] = useState(true);
   const [todos, setTodos] = useState([]);
-
+  const [isError, setIsError] = useState(false);
   useEffect(() => {
     handleGetTodos();
   }, []);
@@ -64,7 +64,9 @@ function Todos() {
       // const results = await Promise.allSettled(requests);
       // do something with results
       await handleGetTodos();
-    } catch (err) {}
+    } catch (err) {
+      setIsError(true);
+    }
   };
 
   const onSubmit = async (title) => {
@@ -81,15 +83,26 @@ function Todos() {
   if (isLoading) {
     return <div>...loading</div>;
   }
+  if (isError) {
+    return <div>Error 404</div>;
+  }
   return (
     <div>
       <TodoInput onSubmit={onSubmit} />
 
       <div>
         {todos.map((item) => (
-          <div key={item.id}>
-            <div>{item.title}</div>
-            <div>{item.status ? "DONE" : "PENDING"}</div>
+          <div key={item.id} style={{ padding: "20px", marginTop: "30px" }}>
+            <span
+              style={{ padding: "30px", color: "white", background: "black" }}
+            >
+              {item.title}
+            </span>
+            <span
+              style={{ padding: "30px", color: "white", background: "black" }}
+            >
+              {item.status ? "DONE" : "PENDING"}
+            </span>
             {/* <button onClick={markEverythingAsComplete(item.id)}> */}
             {/* MARK AS DONE */}
             {/* </button> */}
